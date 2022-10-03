f90b79fa210b30b055e9d68d9a01e5c32d9f1cfd
nelsonfreitas <nelsonfreitas@ipvc.pt>
2022-10-03 17:17:13 +0100

    add1

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -45,10 +45,11 @@ class MainActivity : AppCompatActivity() {
 fun main () {
     val myFirstDice = Dice()
     println(myFirstDice.sides)
+    myFirstDice.roll()
 }
 class Dice {
     var sides = 6
     fun roll (){
-        
-    }
+        val randomNumber = (1..6).random()
+        println(randomNumber)    }
 }
